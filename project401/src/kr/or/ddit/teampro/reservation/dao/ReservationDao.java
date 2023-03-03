c5506cb3313c7df323bae4d5b7cf0854ab503854
Irus0-0 <ikaros9313@gmail.com>
2023-03-03 22:00:42 +0900

    [2023-03-03] 예약 기능 구현중
    현재 데이터 인서트 테스트중

---
+++ project401/src/kr/or/ddit/teampro/reservation/dao/ReservationDao.java
@@ -0,0 +1,53 @@
+package kr.or.ddit.teampro.reservation.dao;
+
+import kr.or.ddit.teampro.reservation.vo.ReservationVo;
+
+import java.util.List;
+
+public interface ReservationDao {
+
+    /**
+     * 예약 정보 넣기
+     * @param reservation 예약 VO 객체
+     * @return DB 작업 성공시 1 반환 실패시 0 반환
+     */
+    int insertReservation(ReservationVo reservation);
+
+    /**
+     * 예약정보 수정하기
+     * @param reservation 예약 VO 객체
+     * @return DB 작업 성공시 1 반환 실패시 0 반환
+     */
+    int updateReservation(ReservationVo reservation);
+
+    /**
+     * 예약정보 삭제하기
+     * @param reservationNum 예약 번호
+     * @return DB 작업 성공시 1 반환 실패시 0 반환
+     */
+    int deleteReservation(String reservationNum);
+
+
+    /**
+     * 사용자 예약 모든 예약정보 가져오기
+     * @param customerId 사용자ID
+     * @return 사용자의 예약정보 LIST로 반환
+     */
+    List<ReservationVo> selectUserAllReservation(String customerId);
+
+    /**
+     * 해당 기업의 모든 예약정보 가져오기
+     * @param companyId 기업 ID
+     * @return 기업이 소유한 모든 숙박시설의 예약정보를 LIST로 반환
+     */
+    List<ReservationVo> selectCoAllReservation(String companyId);
+
+    /**
+     * 숙박시설의 모든 예약정보 가져오기
+     * @param accomKey 0번 숙박시설명 1번 기업ID
+     * @return 선택한 숙박시설의 모든 예약정보를 LIST로 반환
+     */
+    List<ReservationVo> selectAccomAllReservation(List<String> accomKey);
+
+
+}
---
+++ project401/src/kr/or/ddit/teampro/reservation/vo/ReservationVo.java
@@ -0,0 +1,80 @@
+package kr.or.ddit.teampro.reservation.vo;
+
+import java.util.Date;
+
+public class ReservationVo {
+    private String reservationNum; // 예약번호
+    private String customerId; // 고객ID
+    private int roomNumber; // 방번호
+    private String accomName; // 숙박시설명
+    private String companyId; // 기업ID
+    private Date startDate; // 예약 날짜
+    private Date endDate; // 예약 종료날짜
+    private int pepleNum; // 예약인원수
+
+    // Getter Setter
+
+    public String getReservationNum() {
+        return reservationNum;
+    }
+
+    public void setReservationNum(String reservationNum) {
+        this.reservationNum = reservationNum;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getAccomName() {
+        return accomName;
+    }
+
+    public void setAccomName(String accomName) {
+        this.accomName = accomName;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getPepleNum() {
+        return pepleNum;
+    }
+
+    public void setPepleNum(int pepleNum) {
+        this.pepleNum = pepleNum;
+    }
+}
